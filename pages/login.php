e8f91335d3fe4f49e98b8233968b9ea7c10812b5
kircheson <kirill.kotlyaren@mail.ru>
2023-12-09 20:40:44 +0500

    Ничего не работает. Ничего не понятно

--- pages/login.php
+++ pages/login.php
@@ -1,48 +1,51 @@
 <?php
-// Проверяем, была ли отправлена форма
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
-    // Подключение к базе данных (здесь предполагается, что у вас уже создано подключение к базе данных)
-    $conn = new mysqli("localhost", "username", "password", "reg_form_homework");
+session_start();
+//require_once __DIR__ . '../src/helper_functions.php';
+//require_once __DIR__ . '../src/actions/register.php';
+$_GET['auth'] = 0;
 
-    // Проверка подключения
-    if ($conn->connect_error) {
-        die("Ошибка подключения: " . $conn->connect_error);
-    }
-
-    // Получение введенного логина и пароля из формы
-    $login = $_POST["login"];
-    $password = $_POST["password"];
-
-    // Защита от SQL-инъекций
-    $login = $conn->real_escape_string($login);
-    $password = $conn->real_escape_string($password);
-
-    // Подготовка SQL-запроса для проверки логина и пароля в базе данных
-    $sql = "SELECT * FROM users WHERE login='$login' AND password='$password'";
-
-    // Выполнение SQL-запроса
-    $result = $conn->query($sql);
-
-    // Проверка, найден ли пользователь с такими логином и паролем
-    if ($result->num_rows > 0) {
-        // Пользователь авторизован
-        echo "Пользователь успешно авторизован!";
-    } else {
-        // Неправильный логин или пароль
-        echo "Неправильный логин или пароль";
-    }
-
-    // Закрываем соединение с базой данных
-    $conn->close();
-}
-?>
+//// Проверяем, была ли отправлена форма
+//if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
+//    // Подключение к базе данных (здесь предполагается, что у вас уже создано подключение к базе данных)
+//
+//    // Проверка подключения
+//    $link = mysqli_connect("localhost", "username", "password", "reg_form_homework");
+//
+//    if ($link == false) {
+//        print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+//    }
+//
+//    // Получение введенного логина и пароля из формы
+//    $login = $_POST["login"];
+//    $password = $_POST["password"];
+//
+//    // Подготовка SQL-запроса для проверки логина и пароля в базе данных
+//    $sql = "SELECT * FROM users WHERE login='$login' AND password='$password'";
+//
+//    // Выполнение SQL-запроса
+//    $result = $link->query($sql);
+//
+//    // Проверка, найден ли пользователь с такими логином и паролем
+//    if ($result->num_rows > 0) {
+//        // Пользователь авторизован
+////        записать в $_SESSION логин и сам факт того, что он авторизован
+//        echo "Пользователь успешно авторизован!";
+//    } else {
+//        // Неправильный логин или пароль
+//        echo "Неправильный логин или пароль";
+//    }
+//
+//    // Закрываем соединение с базой данных
+//    $conn->close();
+//}
+//?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Авторизация</title>
     <link rel="stylesheet" href="../styles/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -53,7 +56,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
 </head>
 
 <body>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<form method="POST" action="">
+    <?php if ( $_SESSION['auth']) { ?>
+        <h1> Вы <?php echo $_SESSION['login'] ?> успешно авторизовались</h1>
+    <?php } else { ?>
+    <?php if ( $_GET['auth'] ) { ?>
     <div class="container">
         <h1 class="container__title">ВХОД</h1>
 
@@ -70,9 +77,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
         </div>
 
         <div class="container__registration">
-            <a class="container__registration-link" href="../pages/registration.html">У меня нет аккаунта</a>
+            <a class="container__registration-link" href="./pages/registration.php">У меня нет аккаунта</a>
         </div>
     </div>
 </body>
 </html>
-    
